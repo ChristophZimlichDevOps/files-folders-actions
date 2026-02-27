3b31a7c5271730dc77515746d54487e3eefea934
ChristophZimlich <christoph.zimlich@gmail.com>
2026-02-27 17:11:00 +0100

    modified:   FilesFoldersActions.cp.pid.rm.sh

--- FilesFoldersActions.cp.pid.rm.sh
+++ FilesFoldersActions.cp.pid.rm.sh
@@ -79,7 +79,7 @@ OUTPUT_SWITCH=$5
 VERBOSE_SWITCH=$6
 
 ## Set the job config FILE from parameter
-config_file_in="$HOME/bin/linux/shell/FilesFoldersActions.loc/$file_name.conf.in"
+config_file_in="$HOME/bin/linux/shell/local/FilesFoldersActions/$file_name.conf.in"
 echo "Using config file $config_file_in for $file_name_full"
 
 ## Import stuff from config file
@@ -161,13 +161,13 @@ fi
 ## for test purposes it may intermittently update the ports.tcp & ports.udp files unnecessarily. So here we check no other instance
 ## is running. If another instance is running we pause for 5 seconds and then delete the PID anyway. Normally this script will run
 ## in under 150ms and via cron runs every 60 seconds, so if the PID file still exists after 5 seconds then it's a orphan PID file.
-if [ -f "$PID_PATH_FULL" ]; then
-        sleep 5 #if PID file exists wait 5 seconds and test again, if it still exists delete it and carry on
-        echo "There appears to be another Process $file_name PID $PID_PATH_FULL is already running, waiting for 5 seconds ..."
+#if [ -f "$PID_PATH_FULL" ]; then
+        #sleep 5 #if PID file exists wait 5 seconds and test again, if it still exists delete it and carry on
+        #echo "There appears to be another Process $file_name PID $PID_PATH_FULL is already running, waiting for 5 seconds ..."
         #rm -f -- "$PID_PATH_FULL"
-fi
-trap 'rm -f -- $PID_PATH_FULL' EXIT #EXIT status=0/SUCCESS
-echo "$PID" > "$PID_PATH_FULL"
+#fi
+#trap 'rm -f -- $PID_PATH_FULL' EXIT #EXIT status=0/SUCCESS
+#echo "$PID" > "$PID_PATH_FULL"
 
 if [ ! -f "$PID_PATH_FULL" ]; then
         echo "PID file $PID_PATH_FULL not found. EXIT"
@@ -235,10 +235,15 @@ if [ $VERBOSE_SWITCH -eq '1' ]; then
 fi
 
 ## Lets roll
-if [ -f "$PID_PATH_FULL" ]; then
+## If no PID exists...
+if [ ! -f "$PID_PATH_FULL" ]; then
+        echo "PID file $PID_PATH_FULL NOT found"
+        echo "Nothing to do. EXIT"
+        exit 1
+else
+        ## If PID exists...
         ## Get content of PID file
-        readarray -t pids < <(cat "$PID_PATH_FULL" | grep "$PID")
-        #pids=$(echo "$pids_tmp" | grep "$PID")
+        readarray -t pids < <(cat "$PID_PATH_FULL" | grep $PID)
         if [ $VERBOSE_SWITCH -eq '1' ]; then
 
                 for pid in "${!pids[@]}"
@@ -249,6 +254,12 @@ if [ -f "$PID_PATH_FULL" ]; then
         ## PID not found in PID file
 	if [ ${#pids[@]} -eq '0' ]; then
                 echo "NO match found...in PID File $PID_PATH_FULL with PID $PID"
+        elif [ ${#pids[@]} -gt '1' ]; then
+                echo "Something is wrong here..."
+                echo "I found more then 1 entry in PID file $PID_PATH_FULL with PID $PID"
+                echo "PLEASE check this. THANKS"
+                echo "I will now quit"
+                exit 1
         else
                 ## PID found in PID file
                 if [ $VERBOSE_SWITCH -eq '1' ]; then
@@ -256,8 +267,10 @@ if [ -f "$PID_PATH_FULL" ]; then
                         echo "Removing entry in PID File $PID_PATH_FULL with PID $PID started"
                 fi
 		## Removing PID entry in PID file
-                #grep -v "$PID" $PID_PATH_FULL > "/tmp/$file_name-$PID.tmp" && mv -f "/tmp/$file_name-$PID.tmp" $PID_PATH_FULL
-                trap 'sed -i -- '/'$PID'/d' $PID_PATH_FULL'  EXIT #EXIT status=0/SUCCESS
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>     HERE IS THE BUG     <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                #trap 'sed -i -- '/'$PID'/d' $PID_PATH_FULL'  exit
+                sed -i -- '/'$PID'/d' "$PID_PATH_FULL"
                 ## Check last task for errors
                 status=$?
                 if [ $status != 0 ]; then
@@ -265,18 +278,24 @@ if [ -f "$PID_PATH_FULL" ]; then
                         echo "!!! Sub Module $file_name_full v$version stopped with error(s) !!!"
                         exit $status
                 else
-                        if [ $VERBOSE_SWITCH -eq '1' ]; then
-                                echo "Removing entry in PID File $PID_PATH_FULL with PID $PID finished"
+                        ## Check if the job has worked correctly
+                        readarray -t pids_tmp < <(cat "$PID_PATH_FULL" | grep $PID)
+                        if [ ${#pids_tmp[@]} -eq '0' ]; then
+                                if [ $VERBOSE_SWITCH -eq '1' ]; then
+                                        echo "Removing entry in PID File $PID_PATH_FULL with PID $PID finished successfully"
+                                fi
                         fi
-			## Check if PID file has mor content
+			## Check if PID file has more content
                         readarray -t pids_tmp < <(cat "$PID_PATH_FULL")
                 	if [ ${#pids_tmp[@]} -eq '0' ]; then
                         	## If PID file is empty
 				if [ $VERBOSE_SWITCH -eq '1' ]; then 
                                         echo "$PID_PATH_FULL is now empty... Deleting it"
-					trap 'rm -f -v -- $PID_PATH_FULL' EXIT #exit 0
+					#trap 'rm -f -v -- $PID_PATH_FULL' EXIT #exit 0
+                                        rm -f -v "$PID_PATH_FULL"
 				else
-					trap 'rm -f -- $PID_PATH_FULL' EXIT #exit 0
+					#trap 'rm -f -- $PID_PATH_FULL' EXIT #exit 0
+                                        rm -f "$PID_PATH_FULL"
 				fi
                 		## Check last task for errors
                 		status=$?
@@ -292,11 +311,6 @@ if [ -f "$PID_PATH_FULL" ]; then
                 	fi
 		fi
 	fi
-## If no PID exists...
-else
-        echo "PID $PID_PATH_FULL NOT found"
-        echo "Nothing to do. EXIT"
-        exit 1
 fi
 
 if [ $VERBOSE_SWITCH -eq '1' ]; then
